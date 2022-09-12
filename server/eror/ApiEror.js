a6fea7f4c4930e69158437027a820176d1b05eb9
PogunGun <pavel.pogulailo@gmail.com>
2022-09-12 16:07:47 +0300

    server part

---
+++ server/eror/ApiEror.js
@@ -0,0 +1,17 @@
+class ApiError extends Eror{
+    constructor(status,message) {
+        super();
+        this.status=status
+        this.message=message
+    }
+
+    static badRequest(message){
+        return new ApiError(404,message)
+    }
+    static internal(message){
+        return new ApiError(500,message)
+    }
+    static forbiden(message){
+        return new ApiError(403,message)
+    }
+}
